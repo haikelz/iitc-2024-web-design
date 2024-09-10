4eae19e14944ae96de87dd5e2d286cfc09cf8fb4
repiyann <muhrepiyan26@gmail.com>
2024-09-10 12:23:45 +0700

    feat(faq): add new design for faq section (wip)

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -2,10 +2,24 @@ import { Heading, Paragraph } from "./ui/typography";
 
 export default function Faq() {
   return (
-    <section>
-      <div>
-        <Heading as="h2">Frequently Asked Question</Heading>
-        <Paragraph>Quick answers to questions you may here</Paragraph>
+    <section className="flex justify-center">
+      <div className="flex flex-col">
+        <div className="text-center">
+          <Heading as="h2">Frequently Asked Question</Heading>
+          <Paragraph>Quick answers to questions you may here</Paragraph>
+        </div>
+        <div className="mt-7">
+          <Heading as="h4">What is EduVerse?</Heading>
+        </div>
+        <div className="mt-7">
+          <Heading as="h4">Who can use this platform?</Heading>
+        </div>
+        <div className="mt-7">
+          <Heading as="h4">What is a Flash Quiz?</Heading>
+        </div>
+        <div className="mt-7">
+          <Heading as="h4">What are the learning materials available?</Heading>
+        </div>
       </div>
     </section>
   );
