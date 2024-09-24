2316cd3f64ab6ae62761854d68419188083e4932
repiyann <muhrepiyan26@gmail.com>
2024-09-24 15:50:46 +0700

    feat(score): new page for quiz score

--- src/pages/dashboard/quiz/score/index.tsx
+++ src/pages/dashboard/quiz/score/index.tsx
@@ -1,9 +1,26 @@
-import DashboardLayout from "@/components/DashboardLayout";
+import Button from "@/components/ui/Button";
+import { Heading, Paragraph } from "@/components/ui/typography";
 
 export default function Score() {
   return (
-    <DashboardLayout>
-      <section></section>
-    </DashboardLayout>
+    <main className="flex flex-col justify-center items-center min-h-screen">
+      <div className="grid grid-cols-2 gap-40">
+        <div className="flex flex-col text-center">
+          <Heading as="h2">Rank</Heading>
+          <Heading as="h1">7/50</Heading>
+        </div>
+        <div className="flex flex-col text-center">
+          <Heading as="h2">Score</Heading>
+          <Heading as="h1">16/20</Heading>
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <Paragraph>
+          Congratulations on successfully completing the quiz
+        </Paragraph>
+        <Button className="rounded-md">Do it again</Button>
+        <Button className="rounded-md">Leaderboard</Button>
+      </div>
+    </main>
   );
 }
